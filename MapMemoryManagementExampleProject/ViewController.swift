af0ac9177f3875c7f5978053702fa5d3424e174a
Zach Simone <hi@zachsim.one>
2020-10-10 09:42:12 +1100

    Initial Commit

---
+++ MapMemoryManagementExampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapMemoryManagementExampleProject
+//
+//  Created by Zachary Simone on 10/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
