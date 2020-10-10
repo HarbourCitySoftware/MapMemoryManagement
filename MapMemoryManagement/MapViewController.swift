1e7d0ed31baabd0be172d39543edcbd77b665626
Zach Simone <hi@zachsim.one>
2020-10-10 11:00:12 +1100

    Attempt 1 to reduce memory usage.
    - Remove map from view hierarchy when app is backgrounded
    - Add back when app is foregrounded.
    
    Note, memory usage still remains high.

--- MapMemoryManagement/MapViewController.swift
+++ MapMemoryManagement/MapViewController.swift
@@ -5,10 +5,12 @@
 //  Created by Zachary Simone on 10/10/20.
 //
 
+import Combine
 import MapKit
 import UIKit
 
 final class MapViewController: UIViewController {
+    private var cancellable: Set<AnyCancellable> = .init()
 
     private let mapView: MKMapView = {
         let map = MKMapView()
@@ -21,11 +23,31 @@ final class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addMap()
+
+        NotificationCenter.default
+            .publisher(for: UIApplication.didEnterBackgroundNotification)
+            .sink { [weak self] _ in self?.removeMap() }
+            .store(in: &cancellable)
+
+        NotificationCenter.default
+            .publisher(for: UIApplication.didBecomeActiveNotification)
+            .sink { [weak self] _ in self?.addMap() }
+            .store(in: &cancellable)
+
+    }
+
+    private func addMap() {
+        removeMap() // Ensure map isn't already in view hierarchy before trying to add
         view.addSubview(mapView)
         mapView.translatesAutoresizingMaskIntoConstraints = false
         mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+
+    private func removeMap() {
+        mapView.removeFromSuperview()
     }
 }
